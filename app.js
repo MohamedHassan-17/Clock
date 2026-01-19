6e0fa1e9e883cba0160ee8c336df5e77a443abe9
MohamedHassan-17 <mohammed.hassan@emtechhouse.com>
2026-01-19 18:58:40 +0300

    connected to sql

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
+const mysql = require('mysql2');
+
+
+
 const express = require('express');
 const app = express();
 const PORT = 3000;
+
+
+
+// 1. Configure the connection details
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',           // Your MySQL username
+  password: '1313', // Your MySQL password
+  database: 'stopwatch' // The name of the DB where you created the tables
+});
+
+// 2. Attempt to connect
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database: ' + err.stack);
+    return;
+  }
+  console.log('connected'); 
+});
 app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.send('Express server is running');
@@ -9,3 +32,6 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is live at http://localhost:${PORT}`);
 });
+
+
+
